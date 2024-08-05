b7092efdd6f4efd732ceb6ed51c930ae62221806
Alan-Kuan <kuanhaoan@gmail.com>
2024-08-05 11:49:24 +0800

    feat: bound publisher and subscriber

--- src/pyshoz.cpp
+++ src/pyshoz.cpp
@@ -1,6 +1,42 @@
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/function.h>
+#include <nanobind/stl/string.h>
 
 #include "metadata.hpp"
+#include "node/publisher.hpp"
 #include "node/subscriber.hpp"
 
 namespace nb = nanobind;
+
+NB_MODULE(pyshoz, m) {
+    nb::enum_<DeviceType>(m, "DeviceType")
+        .value("kHost", DeviceType::kHost)
+        .value("kGPU", DeviceType::kGPU);
+
+    nb::class_<Domain>(m, "Domain")
+        .def(nb::init<DeviceType, uint16_t>())
+        .def_rw("dev_type", &Domain::dev_type)
+        .def_rw("dev_id", &Domain::dev_id)
+        .def("__repr__", [](const Domain& self) {
+            std::stringstream ss;
+            ss << "<pyshoz.Domain dev_type: " << self.dev_type
+               << ", dev_id: " << self.dev_id << ">";
+            return ss.str();
+        });
+
+    auto node = nb::class_<Node>(m, "Node").def(
+        nb::init<const char*, size_t, uint16_t>());
+
+    nb::class_<Publisher>(m, "Publisher", node)
+        .def(nb::init<const char*, const char*, const Domain&, size_t>())
+        .def("put", &Publisher::put);
+
+    nb::class_<Subscriber>(m, "Subscriber", node)
+        .def(nb::init<const char*, const char*, const Domain&, size_t>())
+        .def("sub", &Subscriber::sub);
+}
