fdb1480ba2f111208db2258be972fb5b5d8f87a8
costoxff <900131shawn@gmail.com>
2024-08-20 16:44:09 +0800

    feat: add argument 'label' to method setPoint()

--- tests/helpers.hpp
+++ tests/helpers.hpp
@@ -11,18 +11,24 @@ public:
 
     Timer(size_t capacity) : capacity(capacity), size(0) {
         this->time_points = new clock::time_point[capacity];
+        this->labels = new uint16_t[capacity];
     }
     Timer() : Timer(64) {}
-    ~Timer() { delete[] time_points; }
+    ~Timer() {
+        delete[] this->time_points;
+        delete[] this->labels;
+    }
 
     inline void reset() { this->size = 0; }
 
-    inline void setPoint() {
+    inline void setPoint(uint16_t label = 0) {
         if (this->size >= capacity) {
-            std::cerr << "recorder is full" << std::endl;
+            std::cerr << "timer's capacity overflow" << std::endl;
             exit(1);
         }
-        time_points[size++] = clock::now();
+        this->time_points[size] = clock::now();
+        this->labels[size] = label;
+        size++;
     }
 
     // dump all time points to a file
@@ -35,7 +41,7 @@ public:
         for (size_t i = 0; i < this->size; i++) {
             std::chrono::duration<double, std::milli> dur =
                 this->time_points[i].time_since_epoch();
-            logfile << dur.count() << std::endl;
+            logfile << this->labels[i] << "," << dur.count() << std::endl;
         }
 
         logfile.close();
@@ -45,6 +51,7 @@ private:
     size_t capacity;
     size_t size;
     clock::time_point* time_points;
+    uint16_t* labels;
 };
 
 }  // namespace hlp
