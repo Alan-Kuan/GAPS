34b27590f8ae14b4f770d7ccae42c72c8f6049ac
Alan-Kuan <kuanhaoan@gmail.com>
2024-08-29 22:35:55 +0800

    chore: update example

--- examples/cross_process_torch.py
+++ examples/cross_process_torch.py
@@ -21,8 +21,7 @@ def main():
         run_as_subscriber()
 
 def run_as_publisher():
-    domain = pyshoz.Domain(pyshoz.DeviceType.kGPU, 0)
-    publisher = pyshoz.Publisher("cross_process_torch", LLOCATOR, domain, POOL_SIZE)
+    publisher = pyshoz.Publisher("cross_process_torch", LLOCATOR, POOL_SIZE)
 
     t = torch.tensor(range(64), dtype=torch.int)
     publisher.put(t)
@@ -31,8 +30,7 @@ def run_as_publisher():
     publisher.put(t)
 
 def run_as_subscriber():
-    domain = pyshoz.Domain(pyshoz.DeviceType.kGPU, 0)
-    subscriber = pyshoz.Subscriber("cross_process_torch", LLOCATOR, domain, POOL_SIZE)
+    subscriber = pyshoz.Subscriber("cross_process_torch", LLOCATOR, POOL_SIZE)
 
     def msg_handler(tensor):
         print(tensor * 2)
