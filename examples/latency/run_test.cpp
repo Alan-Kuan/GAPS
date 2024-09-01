38d9276a635ed3a77d41510aa5b8453787e77105
Alan-Kuan <kuanhaoan@gmail.com>
2024-09-01 12:19:42 +0800

    test: update latency test to support multi-nodes

--- examples/latency/run_test.cpp
+++ examples/latency/run_test.cpp
@@ -1,12 +1,18 @@
+#include <signal.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
+#include <csignal>
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <utility>
+#include <vector>
 
+#include <cuda_runtime.h>
 #include <zenoh.hxx>
 
 #include "helpers.hpp"
@@ -16,8 +22,8 @@
 using namespace std;
 using namespace hlp;
 
-void pubTest(const char* output_name, size_t size, size_t times);
-void subTest(const char* output_name);
+void pubTest(int nproc, const char* output_name, size_t size, size_t times);
+void subTest(int nproc, const char* output_name);
 
 const char kTopic[] = "latency-test";
 const char kDftLLocator[] = "udp/224.0.0.123:7447#iface=lo";
@@ -25,10 +31,12 @@ constexpr size_t kPoolSize = 2 * 1024 * 1024;  // 2 MiB
 
 int main(int argc, char* argv[]) {
     if (argc < 2 || (stoi(argv[1]) == 0 && argc < 4)) {
-        cerr << "Usage: " << argv[0] << " TYPE OUTPUT [SIZE] [TIMES]\n\n"
+        cerr << "Usage: " << argv[0] << " TYPE NPROC OUTPUT [SIZE] [TIMES]\n\n"
              << "TYPE:\n"
              << "  0    publisher\n"
              << "  1    subscriber\n"
+             << "NPROC:\n"
+             << "  number of publishers / subscribers\n"
              << "OUTPUT:\n"
              << "  name of the output csv\n"
              << "SIZE:\n"
@@ -41,17 +49,18 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     int node_type = stoi(argv[1]);
-    const char* output_name = argv[2];
+    int nproc = stoi(argv[2]);
+    const char* output_name = argv[3];
     size_t size, times;
 
     switch (node_type) {
     case 0:
-        size = stoul(argv[3]);
-        times = stoul(argv[4]);
-        pubTest(output_name, size, times);
+        size = stoul(argv[4]);
+        times = stoul(argv[5]);
+        pubTest(nproc, output_name, size, times);
         break;
     case 1:
-        subTest(output_name);
+        subTest(nproc, output_name);
         break;
     default:
         cerr << "Unknown type" << endl;
@@ -61,21 +70,39 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void pubTest(const char* output_name, size_t size, size_t times) {
+void pubTest(int nproc, const char* output_name, size_t size, size_t times) {
     cout << "size: " << size << endl;
     cout << "times: " << times << endl;
 
-    Timer timer(10000);
+    pid_t pid = -1;
+    int p = 1;
+
+    for (; p < nproc; p++) {
+        pid = fork();
+        if (pid < 0) {
+            cerr << "Failed to fork" << endl;
+            exit(1);
+        } else if (pid == 0) {
+            break;
+        }
+    }
+
+    Timer timer(times);
 
     try {
         Publisher pub(kTopic, kDftLLocator, kPoolSize);
+        int count = size / sizeof(int);
+        int* arr = new int[count];
 
-        char* arr = new char[size];
-        for (int i = 0; i < size; i++) arr[i] = rand() % 10;
+        for (int t = 0; t < times; t++) {
+            int tag = (p - 1) * times + t;
+            memset(arr, rand() % 256, size);
+            arr[0] = tag;
 
-        for (int i = 0; i < times; i++) {
-            timer.setPoint();
+            timer.setPoint(tag);
             pub.put(arr, size);
+            // another time point is set at the subscriber-end
+
             // NOTE: iceoryx subscriber may miss messages if publishes too
             // frequently
             usleep(50000);  // 50ms
@@ -91,21 +118,52 @@ void pubTest(const char* output_name, size_t size, size_t times) {
     }
 
     stringstream ss;
-    ss << "pub-" << output_name << ".csv";
+    ss << "pub-" << output_name << '-' << p << ".csv";
     timer.dump(ss.str().c_str());
+
+    if (pid == 0) return;
+    for (int i = 1; i < nproc; i++) wait(nullptr);
 }
 
-void subTest(const char* output_name) {
+void subTest(int nproc, const char* output_name) {
+    vector<pid_t> pids;
+    pid_t pid = -1;
+    int p = 1;
+
+    for (; p < nproc; p++) {
+        pid = fork();
+        pids.push_back(pid);
+        if (pid < 0) {
+            cerr << "Failed to fork" << endl;
+            exit(1);
+        } else if (pid == 0) {
+            break;
+        }
+    }
+
     Timer timer(10000);
 
     try {
         Subscriber sub(kTopic, kDftLLocator, kPoolSize);
+        auto handler = [&timer](void* msg, size_t size) {
+            // upon received, get the current time point
+            auto now = timer.now();
 
-        auto handler = [&timer](void* msg, size_t size) { timer.setPoint(); };
+            // NOTE: though the tag has been copied to the memory synchronously
+            // before publishing, for some reason, if we read it too quickly, we
+            // will read old data
+            usleep(1000);
 
+            // find the tag from the message
+            int* buf = (int*) malloc(size);
+            cudaMemcpy(buf, msg, size, cudaMemcpyDeviceToHost);
+            timer.setPoint(std::move(now), buf[0]);
+            cout << "Saved point " << buf[0] << endl;
+            free(buf);
+        };
         sub.sub(handler);
 
-        cout << "Ctrl+C to leave" << endl;
+        if (pid != 0) cout << "Ctrl+C to leave" << endl;
         hlp::waitForSigInt();
     } catch (zenoh::ErrorMessage& err) {
         cerr << "Zenoh: " << err.as_string_view() << endl;
@@ -115,7 +173,12 @@ void subTest(const char* output_name) {
         exit(1);
     }
 
+    for (pid_t child_pid : pids) kill(child_pid, SIGINT);
+
     stringstream ss;
-    ss << "sub-" << output_name << ".csv";
+    ss << "sub-" << output_name << '-' << p << ".csv";
     timer.dump(ss.str().c_str());
+
+    if (pid == 0) return;
+    for (int i = 1; i < nproc; i++) wait(nullptr);
 }
