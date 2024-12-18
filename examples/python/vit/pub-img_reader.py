72b4d4f03a1fce532da6981fa331888b6d11f3a6
Alan-Kuan <kuanhaoan@gmail.com>
2024-12-18 11:43:17 +0800

    chore: move codes into main functions

--- examples/python/vit/pub-img_reader.py
+++ examples/python/vit/pub-img_reader.py
@@ -14,36 +14,40 @@ LLOCATOR = "udp/224.0.0.123:7447#iface=lo"
 POOL_SIZE = 12 * 1024 * 1024  # 12 MiB
 DEVICE = "cuda"
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} IMG_DIR MAX_IMG_COUNT")
-    exit(1)
+def main():
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} IMG_DIR MAX_IMG_COUNT")
+        exit(1)
 
-img_dir_path = pathlib.Path(sys.argv[1])
-max_img_count = int(sys.argv[2])
+    img_dir_path = pathlib.Path(sys.argv[1])
+    max_img_count = int(sys.argv[2])
 
-# Start measuring
-beg = time.time()
+    # Start measuring
+    beg = time.time()
 
-session = pyshoz.ZenohSession(LLOCATOR)
-publisher = pyshoz.Publisher(session, TOPIC, POOL_SIZE)
+    session = pyshoz.ZenohSession(LLOCATOR)
+    publisher = pyshoz.Publisher(session, TOPIC, POOL_SIZE)
 
-img_paths = sorted(os.listdir(img_dir_path))
-for file in img_paths[:max_img_count]:
-    img_path = img_dir_path / file
-    img = cv2.imread(str(img_path))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_tensor = v2.functional.to_image(img).to(DEVICE)
+    img_paths = sorted(os.listdir(img_dir_path))
+    for file in img_paths[:max_img_count]:
+        img_path = img_dir_path / file
+        img = cv2.imread(str(img_path))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_tensor = v2.functional.to_image(img).to(DEVICE)
 
-    transforms = v2.Compose([
-        v2.Resize(544),
-        v2.CenterCrop(512),
-        v2.ToDtype(torch.float32, scale=True),
-    ])
-    proc_tensor = transforms(img_tensor)
+        transforms = v2.Compose([
+            v2.Resize(544),
+            v2.CenterCrop(512),
+            v2.ToDtype(torch.float32, scale=True),
+        ])
+        proc_tensor = transforms(img_tensor)
 
-    buf_tensor = publisher.malloc(3, (3, 512, 512), (2, 32, 1), False)
-    publisher.copy_tensor(buf_tensor, proc_tensor)
-    publisher.put(buf_tensor)
-    time.sleep(0.1)
+        buf_tensor = publisher.malloc(3, (3, 512, 512), (2, 32, 1), False)
+        publisher.copy_tensor(buf_tensor, proc_tensor)
+        publisher.put(buf_tensor)
+        time.sleep(0.1)
 
-print(f"Beginning time: {beg}")
+    print(f"Beginning time: {beg}")
+
+if __name__ == '__main__':
+    main()
--- examples/python/vit/sub-classify.py
+++ examples/python/vit/sub-classify.py
@@ -14,37 +14,41 @@ LLOCATOR = "udp/224.0.0.123:7447#iface=lo"
 POOL_SIZE = 12 * 1024 * 1024  # 12 MiB
 DEVICE = "cuda"
 
-if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} EXPECT_IMG_NUM")
-    exit(1)
-expect_img_num = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} EXPECT_IMG_NUM")
+        exit(1)
+    expect_img_num = int(sys.argv[1])
 
-signal.signal(signal.SIGINT, lambda _sig, _frame: print("Stopped"))
+    signal.signal(signal.SIGINT, lambda _sig, _frame: print("Stopped"))
 
-model_name = "apple/mobilevit-small"
-model = MobileViTForImageClassification.from_pretrained(model_name)
-model.to(DEVICE)
-model.eval()
+    model_name = "apple/mobilevit-small"
+    model = MobileViTForImageClassification.from_pretrained(model_name)
+    model.to(DEVICE)
+    model.eval()
 
-preds = []
-timepoints = []
+    preds = []
+    timepoints = []
 
-def handler(inputs):
-    with torch.no_grad():
-        logits = model(pixel_values=inputs.unsqueeze(0)).logits
-    pred = logits.argmax(-1).item()
-    preds.append(pred)
-    timepoints.append(time.time())
-    if len(timepoints) == expect_img_num:
-        os.kill(os.getpid(), signal.SIGINT)
+    def handler(inputs):
+        with torch.no_grad():
+            logits = model(pixel_values=inputs.unsqueeze(0)).logits
+        pred = logits.argmax(-1).item()
+        preds.append(pred)
+        timepoints.append(time.time())
+        if len(timepoints) == expect_img_num:
+            os.kill(os.getpid(), signal.SIGINT)
 
-session = pyshoz.ZenohSession(LLOCATOR)
-_subscriber = pyshoz.Subscriber(session, TOPIC, POOL_SIZE, handler)
+    session = pyshoz.ZenohSession(LLOCATOR)
+    _subscriber = pyshoz.Subscriber(session, TOPIC, POOL_SIZE, handler)
 
-print(f"Ready! Expect to receive {expect_img_num} images")
-signal.pause()
+    print(f"Ready! Expect to receive {expect_img_num} images")
+    signal.pause()
 
-for pred in preds:
-    print(model.config.id2label[pred])
+    for pred in preds:
+        print(model.config.id2label[pred])
 
-print(f"Finish time: {timepoints[-1]}")
+    print(f"Finish time: {timepoints[-1]}")
+
+if __name__ == '__main__':
+    main()
