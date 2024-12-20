e1453f9c361bf36af3e9fa5c946531279eb4fdb8
Alan-Kuan <kuanhaoan@gmail.com>
2024-12-20 15:48:09 +0800

    test: add multiple publisher & subscriber test

--- examples/latency/run_test.cpp
+++ examples/latency/run_test.cpp
@@ -1,7 +1,6 @@
 #include <sys/wait.h>
 
 #include <chrono>
-#include <csignal>
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
@@ -9,8 +8,8 @@
 #include <stdexcept>
 #include <string>
 #include <thread>
+#include <utility>
 
-#include <cuda_runtime.h>
 #include <zenoh.hxx>
 
 #include "helpers.hpp"
@@ -19,39 +18,41 @@
 #include "node/subscriber.hpp"
 
 using namespace std;
-using namespace hlp;
-using namespace chrono_literals;
+using namespace std::chrono_literals;
+namespace z = zenoh;
 
 void pubTest(int nproc, const char* output_name, size_t size, size_t times);
 void subTest(int nproc, const char* output_name);
 
 const char kTopic[] = "latency-test";
 const char kDftLLocator[] = "udp/224.0.0.123:7447#iface=lo";
+const auto kPubInterval = 20ms;
 constexpr size_t kPoolSize = 32 * 1024 * 1024;  // 32 MiB
 
 int main(int argc, char* argv[]) {
-    if (argc < 2 || (stoi(argv[1]) == 0 && argc < 4)) {
+    if (argc < 4 || (argv[1][0] == 'p' && argc < 6)) {
         cerr << "Usage: " << argv[0] << " TYPE NPROC OUTPUT [SIZE] [TIMES]\n\n"
              << "TYPE:\n"
-             << "  0    publisher\n"
-             << "  1    subscriber\n"
+             << "  p    publisher\n"
+             << "  s    subscriber\n"
              << "NPROC:\n"
              << "  number of publishers / subscribers\n"
              << "OUTPUT:\n"
              << "  prefix of the output csv (may contain directory)\n"
              << "SIZE:\n"
              << "  size of the message to publish in bytes (only required when "
-                "TYPE=0)\n"
+                "TYPE=p)\n"
              << "TIMES:\n"
              << "  number of times to publish a message (only required when "
-                "TYPE=0)"
+                "TYPE=p)"
              << endl;
-        exit(1);
+        return 1;
     }
-    int node_type = stoi(argv[1]);
+    char node_type = argv[1][0];
     int nproc = stoi(argv[2]);
     const char* output_name = argv[3];
-    size_t size, times;
+    size_t size;
+    int times;
 
 #ifndef NDEBUG
     cout << "The code was compiled without NDEBUG macro defined, which may "
@@ -60,12 +61,12 @@ int main(int argc, char* argv[]) {
 #endif
 
     switch (node_type) {
-    case 0:
+    case 'p':
         size = stoul(argv[4]);
-        times = stoul(argv[5]);
+        times = stoi(argv[5]);
         pubTest(nproc, output_name, size, times);
         break;
-    case 1:
+    case 's':
         subTest(nproc, output_name);
         break;
     default:
@@ -77,9 +78,6 @@ int main(int argc, char* argv[]) {
 }
 
 void pubTest(int nproc, const char* output_name, size_t size, size_t times) {
-    cout << "size: " << size << endl;
-    cout << "times: " << times << endl;
-
     pid_t pid = -1;
     int p = 1;
 
@@ -93,13 +91,13 @@ void pubTest(int nproc, const char* output_name, size_t size, size_t times) {
         }
     }
 
-    Timer timer(times);
+    hlp::Timer timer(times);
 
     try {
-        auto config = zenoh::Config::create_default();
+        auto config = z::Config::create_default();
         config.insert(Z_CONFIG_MODE_KEY, Z_CONFIG_MODE_PEER);
         config.insert(Z_CONFIG_LISTEN_KEY, kDftLLocator);
-        zenoh::Session session(std::move(config));
+        z::Session session(std::move(config));
         Publisher pub(session, kTopic, kPoolSize);
 
         for (int t = 0; t < times; t++) {
@@ -108,7 +106,7 @@ void pubTest(int nproc, const char* output_name, size_t size, size_t times) {
             do {
                 buf_d = (int*) pub.malloc(size);
             } while (!buf_d);
-            init_data(buf_d, size / sizeof(int), tag);
+            init::fillArray(buf_d, size / sizeof(int), tag);
 
             timer.setPoint(tag);
             // since we won't use the data from the subscriber side, it's ok to
@@ -117,7 +115,7 @@ void pubTest(int nproc, const char* output_name, size_t size, size_t times) {
             // another time point is set at the subscriber-end
 
             // control publishing frequency
-            this_thread::sleep_for(20ms);
+            this_thread::sleep_for(kPubInterval);
         }
     } catch (runtime_error& err) {
         cerr << "Publisher: " << err.what() << endl;
@@ -146,13 +144,13 @@ void subTest(int nproc, const char* output_name) {
         }
     }
 
-    Timer timer(10000);
+    hlp::Timer timer(10000);
 
     try {
-        auto config = zenoh::Config::create_default();
+        auto config = z::Config::create_default();
         config.insert(Z_CONFIG_MODE_KEY, Z_CONFIG_MODE_PEER);
         config.insert(Z_CONFIG_LISTEN_KEY, kDftLLocator);
-        zenoh::Session session(std::move(config));
+        z::Session session(std::move(config));
         Subscriber sub(session, kTopic, kPoolSize,
                        [&timer](void* msg, size_t tag) {
                            // upon received, set the current time point
---
+++ scripts/get-results.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+names = ['1KB', '4KB', '16KB', '64KB', '256KB', '1MB', '4MB', '16MB']
+table = {}
+
+def getDiff(pub_csv, sub_csv):
+    diff_nsec = sub_csv[2] - pub_csv[2]
+
+    ltz_idx = diff_nsec < 0
+    diff_nsec[ltz_idx] += 10**9
+    sub_csv[1][ltz_idx] -= 1
+    diff_sec = sub_csv[1] - pub_csv[1]
+
+    return diff_sec * 10**3 + diff_nsec / 10**6
+
+# Calculate 1p1s latencies
+
+for name in names:
+    pub_csv = pd.read_csv(f'outputs/1p1s/pub-{name}-1.csv', header=None)
+    sub_csv = pd.read_csv(f'outputs/1p1s/sub-{name}-1.csv', header=None)
+    table[name] = getDiff(pub_csv, sub_csv)
+
+pd.DataFrame(table).to_csv('results.csv', index=False)
+print('results.csv is generated')
+
+# Calculate mpns latencies
+
+mpns_list = [(2, 1), (4, 1), (8, 1), (1, 2), (1, 4), (1, 8), (4, 4)]
+table = {}
+
+for mpns in mpns_list:
+    name = f'{mpns[0]}p{mpns[1]}s'
+
+    pub_csv_list = []
+    for i in range(mpns[0]):
+        pub_csv_list.append(pd.read_csv(f'outputs/mpns/pub-{name}-{i + 1}.csv', header=None))
+    pub_csv = pd.concat(pub_csv_list)
+
+    diff_list = []
+    for i in range(mpns[1]):
+        sub_csv = pd.read_csv(f'outputs/mpns/sub-{name}-{i + 1}.csv', header=None)
+        sub_csv.sort_values(by=0)
+        diff_list.append(getDiff(pub_csv, sub_csv))
+    table[name] = pd.concat(diff_list)
+
+pd.DataFrame(table).to_csv('results-mpns.csv', index=False)
+print('results-mpns.csv is generated')
