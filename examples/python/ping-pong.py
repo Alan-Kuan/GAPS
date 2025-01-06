15616256aabb5d61bfd16eb9e319c203570c0e36
Alan-Kuan <kuanhaoan@gmail.com>
2025-01-06 16:39:51 +0800

    test: replace arithmetic_torch.pywith ping-pong.py

---
+++ examples/python/ping-pong.py
@@ -0,0 +1,71 @@
+import argparse
+import signal
+import time
+
+import pyshoz
+import torch
+
+TOPIC_PING = "p3-ping"
+TOPIC_PONG = "p3-pong"
+LLOCATOR = "udp/224.0.0.123:7447#iface=lo"
+POOL_SIZE = 2 * 1024 * 1024;  # 2 MiB
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p',
+                        action="store_true",
+                        help="be a publisher or not (if not specify, it becomes a subscriber)")
+    args = parser.parse_args()
+
+    signal.signal(signal.SIGINT, lambda _sig, _frame: print("Stopped"))
+
+    session = pyshoz.ZenohSession(LLOCATOR)
+    if args.p:
+        print("Publisher Mode")
+        run_as_publisher(session)
+    else:
+        print("Subscriber Mode")
+        run_as_subscriber(session)
+
+def run_as_publisher(session):
+    publisher = pyshoz.Publisher(session, TOPIC_PING, POOL_SIZE)
+
+    # int[64]
+    ori_tensor = publisher.malloc(1, (64, ), (0, 32, 1))
+    for i in range(64):
+        ori_tensor[i] = i
+
+    def msg_handler(tensor):
+        if torch.equal(tensor, ori_tensor * 2):
+            print('Passed')
+        else:
+            print('Failed')
+
+    # NOTE: intentionally assign it to a variable, or it destructs right after this line is executed
+    _subscriber = pyshoz.Subscriber(session, TOPIC_PONG, POOL_SIZE, msg_handler)
+
+    # make sure subscriber is ready
+    time.sleep(2)
+
+    publisher.put(ori_tensor)
+
+    print("Ctrl+C to leave")
+    signal.pause()
+
+def run_as_subscriber(session):
+    publisher = pyshoz.Publisher(session, TOPIC_PONG, POOL_SIZE)
+
+    def msg_handler(tensor):
+        buf = publisher.malloc(1, (64, ), (0, 32, 1))
+        publisher.copy_tensor(buf, tensor)
+        buf *= 2
+        publisher.put(buf)
+
+    # NOTE: intentionally assign it to a variable, or it destructs right after this line is executed
+    _subscriber = pyshoz.Subscriber(session, TOPIC_PING, POOL_SIZE, msg_handler)
+
+    print("Ctrl+C to leave")
+    signal.pause()
+
+if __name__ == "__main__":
+    main()
