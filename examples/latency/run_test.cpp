0c66036efed5545879bee9e844aa3c86f6a6f43a
Alan-Kuan <kuanhaoan@gmail.com>
2025-02-19 11:39:36 +0800

    test: delay the creation of publisher/subscriber

--- examples/latency/run_test.cpp
+++ examples/latency/run_test.cpp
@@ -102,6 +102,7 @@ int main(int argc, char* argv[]) {
         } else if (pid == 0) {
             break;
         }
+        this_thread::sleep_for(20ms);
     }
 
     auto config = z::Config::create_default();
