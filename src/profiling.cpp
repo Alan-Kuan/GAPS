f834da85bb2c99211afd6634eac9f46e55740dde
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-01 22:39:45 +0800

    feat: dump raw time points

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -15,11 +15,9 @@ void dump_records(std::string& name, int points_per_group) {
         int j_end = j_beg + points_per_group;
 
         out << records.tags[i];
-        for (int j = j_beg + 1; j < j_end; j++) {
-            double diff =
-                (records.tps[j].tv_sec - records.tps[j - 1].tv_sec) * 1e6 +
-                (records.tps[j].tv_nsec - records.tps[j - 1].tv_nsec) / 1e3;
-            out << ',' << diff;
+        for (int j = j_beg; j < j_end; j++) {
+            out << ',' << records.tps[j].tv_sec << ','
+                << records.tps[j].tv_nsec;
         }
         out << std::endl;
 
