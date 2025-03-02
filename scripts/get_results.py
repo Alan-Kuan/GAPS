e0469f801bc7abbb8aca34c5f9f73ff7e4e52e6c
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-02 21:07:55 +0800

    test: update GAPS latency test scripts

--- scripts/get_results.py
+++ scripts/get_results.py
@@ -1,52 +1,98 @@
 #!/usr/bin/env python3
-from os import path
+import sys
 
 import pandas as pd
 
-SCRIPT_DIR = path.dirname(path.realpath(__file__))
-PROJECT_DIR = path.dirname(SCRIPT_DIR)
-OUTPUT_1_DIR = "outputs/1p1s"
-# OUTPUT_2_DIR = "outputs/mpns"
+#
+#  calculate the end-to-end latency and the latency of each part
+#
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} OUTPUT_DIR")
+    exit(1)
+
+output_dir = sys.argv[1]
+output_dir_1p1s = f"{output_dir}/1p1s"
+output_dir_mpns = f"{output_dir}/mpns"
 
 names = ["1KB", "4KB", "16KB", "64KB", "256KB", "1MB", "4MB"]
 table = {}
 
-def getDiff(pub_csv, sub_csv):
-    diff_nsec = sub_csv[2] - pub_csv[2]
-    diff_sec = sub_csv[1] - pub_csv[1]
-    return diff_sec * 10**6 + diff_nsec / 10**3
+def get_e2e_latency(df_pub, df_sub):
+    diff_sec = df_sub[5] - df_pub[1]
+    diff_nsec = df_sub[6] - df_pub[2]
+    diff = diff_sec * 1e6 + diff_nsec / 1e3
+    return diff
+
+def dump_latency_of_each_part(df, output_prefix):
+    cols = []
+    for i in range(3, df.columns.size, 2):
+        prev = i - 2
+        diff_sec = df[i] - df[prev]
+        diff_nsec = df[i + 1] - df[prev + 1]
+        diff = diff_sec * 1e6 + diff_nsec / 1e3
+        cols.append(diff)
+    res = pd.concat(cols, axis=1)
+    output_name = f"{output_prefix}-breakdown.csv"
+    res.to_csv(output_name, header=False)
+    print(f"{output_name} is generated.")
 
 # Calculate 1p1s latencies
 
+cols_e2e = []
 for name in names:
-    pub_csv = pd.read_csv(f"{OUTPUT_1_DIR}/pub-{name}-1.csv", header=None)
-    sub_csv = pd.read_csv(f"{OUTPUT_1_DIR}/sub-{name}-1.csv", header=None)
-    table[name] = getDiff(pub_csv, sub_csv)
+    pub_prefix = f"{output_dir_1p1s}/pub-{name}"
+    sub_prefix = f"{output_dir_1p1s}/sub-{name}"
 
-output_file = "results.csv"
-pd.DataFrame(table).to_csv(output_file, index=False)
-print(f"{output_file} is generated")
+    df_pub = pd.read_csv(f"{pub_prefix}-1.csv", header=None, index_col=0)
+    df_sub = pd.read_csv(f"{sub_prefix}-1.csv", header=None, index_col=0)
+
+    # end-to-end latency
+    cols_e2e.append(get_e2e_latency(df_pub, df_sub))
+
+    # latency of each part
+    dump_latency_of_each_part(df_pub, pub_prefix)
+    dump_latency_of_each_part(df_sub, sub_prefix)
+
+res = pd.concat(cols_e2e, axis=1)
+res.columns = names
+
+output_name = f"{output_dir_1p1s}/e2e-latency.csv"
+res.to_csv(output_name, index=False)
+print(f"{output_name} is generated.")
 
 # Calculate mpns latencies
 
-# mpns_list = [(2, 1), (4, 1), (8, 1), (1, 2), (1, 4), (1, 8), (4, 4)]
-# table = {}
-#
-# for mpns in mpns_list:
-#     name = f"{mpns[0]}p{mpns[1]}s"
-#
-#     pub_csv_list = []
-#     for i in range(mpns[0]):
-#         pub_csv_list.append(pd.read_csv(f"{OUTPUT_2_DIR}/pub-{name}-{i + 1}.csv", header=None))
-#     pub_csv = pd.concat(pub_csv_list)
-#
-#     diff_list = []
-#     for i in range(mpns[1]):
-#         sub_csv = pd.read_csv(f"{OUTPUT_2_DIR}/sub-{name}-{i + 1}.csv", header=None)
-#         sub_csv.sort_values(by=0)
-#         diff_list.append(getDiff(pub_csv, sub_csv))
-#     table[name] = pd.concat(diff_list)
-#
-# output_file = "results-mpns.csv"
-# pd.DataFrame(table).to_csv(output_file, index=False)
-# print(f"{output_file} is generated")
+mpns_list = [(1, 2), (1, 4), (2, 1), (4, 1), (2, 2)]
+table = {}
+
+for mpns in mpns_list:
+    name = f"{mpns[0]}p{mpns[1]}s"
+
+    df_pub_list = []
+    for i in range(mpns[0]):
+        pub_prefix = f"{output_dir_mpns}/pub-{name}-{i + 1}"
+        df_pub = pd.read_csv(f"{pub_prefix}.csv", header=None, index_col=0)
+
+        # latency of each part
+        dump_latency_of_each_part(df_pub, pub_prefix)
+
+        df_pub_list.append(df_pub)
+    df_pub = pd.concat(df_pub_list).sort_index()
+
+    diff_list = []
+    for i in range(mpns[1]):
+        sub_prefix = f"{output_dir_mpns}/sub-{name}-{i + 1}"
+        df_sub = pd.read_csv(f"{sub_prefix}.csv", header=None, index_col=0)
+
+        # latency of each part
+        dump_latency_of_each_part(df_sub, sub_prefix)
+
+        # end-to-end latency
+        df_sub = df_sub.sort_index()
+        diff_list.append(get_e2e_latency(df_pub, df_sub))
+    table[name] = pd.concat(diff_list)
+
+output_name = f"{output_dir_mpns}/e2e-latency-mpns.csv"
+pd.DataFrame.from_dict(table, orient="index").T.to_csv(output_name, index=False)
+print(f"{output_name} is generated.")
--- scripts/run_latency_test.sh
+++ scripts/run_latency_test.sh
@@ -2,22 +2,23 @@
 
 set -e
 
-SIZE=(1024 4096 16384 65536 262144 1048576 4194304)
-NAME=(1KB 4KB 16KB 64KB 256KB 1MB 4MB)
-TIMES=100
-PUB_INTERVAL=0.02  # 20ms
-
-# NP=(2 4 8 1 1 1 4)
-# NS=(1 1 1 2 4 8 4)
-# SPEC_SIZE="${SIZE[7]}"
-
 SCRIPT_DIR=`dirname $(realpath "$0")`
 RUN_TEST="./build/examples/latency/run_test"
-OUTPUT_1_DIR="outputs/1p1s"
-# OUTPUT_2_DIR="outputs/mpns"
+OUTPUT_DIR="outputs"
+OUTPUT_DIR_1P1S="${OUTPUT_DIR}/1p1s"
+OUTPUT_DIR_MPNS="${OUTPUT_DIR}/mpns"
 
-mkdir -p "${OUTPUT_1_DIR}"
-# mkdir -p "${OUTPUT_2_DIR}"
+SIZE=(1024 4096 16384 65536 262144 1048576 4194304)
+NAME=(1KB 4KB 16KB 64KB 256KB 1MB 4MB)
+TIMES=10
+PUB_INTERVAL=0.0001  # 100 us
+
+NP=(1 1 2 4 2)
+NS=(2 4 1 1 2)
+SPEC_SIZE="${SIZE[0]}"
+
+mkdir -p "${OUTPUT_DIR_1P1S}"
+mkdir -p "${OUTPUT_DIR_MPNS}"
 
 ./build/src/mem_manager >/dev/null &
 MM_PID="$!"
@@ -25,39 +26,48 @@ sleep 1
 
 trap -- "kill -s INT ${MM_PID}" EXIT
 
+echo "Starting GAPS Latency Test"
+echo
+
 echo "1. Testing with different payload sizes"
 echo
 
-for i in {0..6}; do
+for i in "${!SIZE[@]}"; do
     echo "Testing with payload size: ${NAME[i]} ..."
 
-    "${RUN_TEST}" -n 1 -o "${OUTPUT_1_DIR}/sub-${NAME[i]}" >/dev/null &
+    PUB_PREFIX="${OUTPUT_DIR_1P1S}/pub-${NAME[i]}"
+    SUB_PREFIX="${OUTPUT_DIR_1P1S}/sub-${NAME[i]}"
+
+    "${RUN_TEST}" -n 1 -o "${SUB_PREFIX}" >/dev/null &
     SUB_PID="$!"
     sleep 1  # wait a while for the subscriber to be ready
-    "${RUN_TEST}" -p -n 1 -o "${OUTPUT_1_DIR}/pub-${NAME[i]}" -s "${SIZE[i]}" -t "${TIMES}" -i "${PUB_INTERVAL}"
+    "${RUN_TEST}" -n 1 -o "${PUB_PREFIX}" -p -s "${SIZE[i]}" -t "${TIMES}" -i "${PUB_INTERVAL}" >/dev/null
     sleep 1  # wait a while for the subscriber to finish handling
 
     kill -s INT "${SUB_PID}"
     sleep 1  # wait a while for the subscriber to finish dumping
 done
 
-# echo
-# echo "2. Testing with different numbers of publishers and subscribers"
-# echo
-#
-# for i in {0..6}; do
-#     echo "Testing with (p, s) = (${NP[i]}, ${NS[i]}) ..."
-#
-#     "${RUN_TEST}" s "${NS[i]}" "${OUTPUT_2_DIR}/sub-${NP[i]}p${NS[i]}s" >/dev/null &
-#     sleep 1  # wait a while for the subscriber to be ready
-#     "${RUN_TEST}" p "${NP[i]}" "${OUTPUT_2_DIR}/pub-${NP[i]}p${NS[i]}s" "${SPEC_SIZE}"
-#     sleep 1  # wait a while for the subscriber to finish handling
-#
-#     pkill --signal INT run_test
-#     sleep 1  # wait a while for the subscriber to finish dumping
-# done
+echo
+echo "2. Testing with different numbers of publishers and subscribers"
+echo
+
+for i in "${!NP[@]}"; do
+    echo "Testing with (p, s) = (${NP[i]}, ${NS[i]}) ..."
+
+    PUB_PREFIX="${OUTPUT_DIR_MPNS}/pub-${NP[i]}p${NS[i]}s"
+    SUB_PREFIX="${OUTPUT_DIR_MPNS}/sub-${NP[i]}p${NS[i]}s"
+
+    "${RUN_TEST}" -n "${NS[i]}" -o "${SUB_PREFIX}" >/dev/null &
+    sleep 1  # wait a while for the subscriber to be ready
+    "${RUN_TEST}" -n "${NP[i]}" -o "${PUB_PREFIX}" -p -s "${SPEC_SIZE}" -t "${TIMES}" -i "${PUB_INTERVAL}" >/dev/null
+    sleep 1  # wait a while for the subscriber to finish handling
+
+    pkill --signal INT run_test
+    sleep 1  # wait a while for the subscriber to finish dumping
+done
 
 echo
 echo "Done!"
 
-"${SCRIPT_DIR}/get_results.py"
+"${SCRIPT_DIR}/get_results.py" "${OUTPUT_DIR}"
