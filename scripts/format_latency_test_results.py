a58f8ffab5987e05aedcdd4dc47109b789b6dfd9
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-16 21:55:34 +0800

    chore: separate 1p1s and mpns tests into 2 scripts & format mpns in a finer-grained way

---
+++ scripts/format_latency_test_results.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import sys
+
+import pandas as pd
+
+#
+#  calculate the end-to-end latency and the latency of each part
+#
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} OUTPUT_DIR")
+    exit(1)
+
+output_dir = f"{sys.argv[1]}/1p1s"
+names = ["1KB", "4KB", "16KB", "64KB", "256KB", "1MB", "4MB"]
+
+def get_e2e_latency(df_pub, df_sub):
+    diff_sec = df_sub[5] - df_pub[1]
+    diff_nsec = df_sub[6] - df_pub[2]
+    diff = diff_sec * 1e6 + diff_nsec / 1e3
+    return diff
+
+def dump_latency_of_each_part(df, output_prefix):
+    cols = []
+    for i in range(2, df.columns.size, 2):
+        prev = i - 2
+        diff_sec = df.iloc[:, i] - df.iloc[:, prev]
+        diff_nsec = df.iloc[:, i + 1] - df.iloc[:, prev + 1]
+        diff = diff_sec * 1e6 + diff_nsec / 1e3
+        cols.append(diff)
+    res = pd.concat(cols, axis=1)
+    output_name = f"{output_prefix}-breakdown.csv"
+    res.to_csv(output_name, header=False)
+    print(f"{output_name} is generated.")
+
+cols = []
+for name in names:
+    pub_prefix = f"{output_dir}/pub-{name}"
+    sub_prefix = f"{output_dir}/sub-{name}"
+
+    df_pub = pd.read_csv(f"{pub_prefix}-1.csv", header=None, index_col=0)
+    df_sub = pd.read_csv(f"{sub_prefix}-1.csv", header=None, index_col=0)
+
+    # end-to-end latency
+    cols.append(get_e2e_latency(df_pub, df_sub))
+
+    # latency of each part
+    dump_latency_of_each_part(df_pub.drop(columns=[1, 2]), pub_prefix)
+    dump_latency_of_each_part(df_sub.drop(columns=[5, 6]), sub_prefix)
+
+res = pd.concat(cols, axis=1)
+res.columns = names
+
+output_name = f"{output_dir}/e2e-latency.csv"
+res.to_csv(output_name, index=False)
+print(f"{output_name} is generated.")
--- scripts/run_latency_test.sh
+++ scripts/run_latency_test.sh
@@ -4,21 +4,14 @@ set -e
 
 SCRIPT_DIR=`dirname $(realpath "$0")`
 RUN_TEST="./build/examples/latency/run_test"
-OUTPUT_DIR="outputs"
-OUTPUT_DIR_1P1S="${OUTPUT_DIR}/1p1s"
-OUTPUT_DIR_MPNS="${OUTPUT_DIR}/mpns"
+OUTPUT_DIR="outputs/1p1s"
 
 SIZE=(1024 4096 16384 65536 262144 1048576 4194304)
 NAME=(1KB 4KB 16KB 64KB 256KB 1MB 4MB)
 TIMES=100
 PUB_INTERVAL=0.0001  # 100 us
 
-NP=(1 1 2 4 2)
-NS=(2 4 1 1 2)
-SPEC_SIZE="${SIZE[0]}"
-
-mkdir -p "${OUTPUT_DIR_1P1S}"
-mkdir -p "${OUTPUT_DIR_MPNS}"
+mkdir -p "${OUTPUT_DIR}"
 
 ./build/src/mem_manager >/dev/null &
 MM_PID="$!"
@@ -29,14 +22,11 @@ trap -- "kill -s INT ${MM_PID}" EXIT
 echo "Starting GAPS Latency Test"
 echo
 
-echo "1. Testing with different payload sizes"
-echo
-
 for i in "${!SIZE[@]}"; do
     echo "Testing with payload size: ${NAME[i]} ..."
 
-    PUB_PREFIX="${OUTPUT_DIR_1P1S}/pub-${NAME[i]}"
-    SUB_PREFIX="${OUTPUT_DIR_1P1S}/sub-${NAME[i]}"
+    PUB_PREFIX="${OUTPUT_DIR}/pub-${NAME[i]}"
+    SUB_PREFIX="${OUTPUT_DIR}/sub-${NAME[i]}"
 
     "${RUN_TEST}" -n 1 -o "${SUB_PREFIX}" >/dev/null &
     SUB_PID="$!"
@@ -49,25 +39,6 @@ for i in "${!SIZE[@]}"; do
 done
 
 echo
-echo "2. Testing with different numbers of publishers and subscribers"
-echo
-
-for i in "${!NP[@]}"; do
-    echo "Testing with (p, s) = (${NP[i]}, ${NS[i]}) ..."
-
-    PUB_PREFIX="${OUTPUT_DIR_MPNS}/pub-${NP[i]}p${NS[i]}s"
-    SUB_PREFIX="${OUTPUT_DIR_MPNS}/sub-${NP[i]}p${NS[i]}s"
-
-    "${RUN_TEST}" -n "${NS[i]}" -o "${SUB_PREFIX}" >/dev/null &
-    sleep 1  # wait a while for the subscriber to be ready
-    "${RUN_TEST}" -n "${NP[i]}" -o "${PUB_PREFIX}" -p -s "${SPEC_SIZE}" -t "${TIMES}" -i "${PUB_INTERVAL}" >/dev/null
-    sleep 1  # wait a while for the subscriber to finish handling
-
-    pkill --signal INT run_test
-    sleep 1  # wait a while for the subscriber to finish dumping
-done
-
-echo
 echo "Done!"
 
-"${SCRIPT_DIR}/get_results.py" "${OUTPUT_DIR}"
+"${SCRIPT_DIR}/format_latency_test_results.py" "${OUTPUT_DIR}"
